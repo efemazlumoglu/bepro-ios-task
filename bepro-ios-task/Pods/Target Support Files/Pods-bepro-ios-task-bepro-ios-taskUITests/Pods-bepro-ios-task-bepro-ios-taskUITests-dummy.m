6c2f9c81e52e50a10c8477d5583050be6b607e21
efemaz <efe.mazlumoglu@gmail.com>
2022-03-18 17:52:17 +0300

    podfile added for rx swift

---
+++ bepro-ios-task/Pods/Target Support Files/Pods-bepro-ios-task-bepro-ios-taskUITests/Pods-bepro-ios-task-bepro-ios-taskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bepro_ios_task_bepro_ios_taskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_bepro_ios_task_bepro_ios_taskUITests
+@end
---
+++ bepro-ios-task/Pods/Target Support Files/Pods-bepro-ios-taskTests/Pods-bepro-ios-taskTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bepro_ios_taskTests : NSObject
+@end
+@implementation PodsDummy_Pods_bepro_ios_taskTests
+@end
