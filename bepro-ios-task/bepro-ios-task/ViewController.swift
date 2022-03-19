1463aa5b21c4185c0c8f8f51b5e8553cac0296d6
efemaz <efe.mazlumoglu@gmail.com>
2022-03-19 21:28:57 +0300

    initial ui implementation

--- bepro-ios-task/bepro-ios-task/ViewController.swift
+++ bepro-ios-task/bepro-ios-task/ViewController.swift
@@ -24,17 +24,65 @@ class ViewController: UIViewController {
     var firstHalfVideo: Video?
     var secondHalfVideo: Video?
     var videoURL: URL?
+    var listOfOptions: [String] = ["First Half", "Second Half"]
     private let videoPlayer = StreamingVideoPlayer()
+    private var matchIdTextField = UITextField()
+    public var playerView = UIView()
+    public var tableView = UITableView()
+    public var tableViewCell = UITableViewCell()
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // match id fetch
-        // video url fetch
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        super.loadView()
         
+        self.matchIdTextField =  UITextField(frame: CGRect(x: 20, y: 100, width: 200, height: 44))
+        matchIdTextField.placeholder = "Enter Match Id Here"
+        matchIdTextField.font = UIFont.systemFont(ofSize: 15)
+        matchIdTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        matchIdTextField.autocorrectionType = UITextAutocorrectionType.no
+        matchIdTextField.keyboardType = UIKeyboardType.default
+        matchIdTextField.returnKeyType = UIReturnKeyType.done
+        matchIdTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+        matchIdTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        matchIdTextField.delegate = self
+        matchIdTextField.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(matchIdTextField)
+        
+        NSLayoutConstraint.activate([
+            matchIdTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            matchIdTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            matchIdTextField.heightAnchor.constraint(equalTo: matchIdTextField.heightAnchor),
+            matchIdTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+        
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(playerView)
+        
+        NSLayoutConstraint.activate([
+            playerView.topAnchor.constraint(equalTo: matchIdTextField.bottomAnchor, constant: 30),
+            playerView.leadingAnchor.constraint(equalTo: matchIdTextField.leadingAnchor),
+            playerView.heightAnchor.constraint(equalTo: matchIdTextField.heightAnchor, constant: 126),
+            playerView.trailingAnchor.constraint(equalTo: matchIdTextField.trailingAnchor)
+        ])
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: playerView.bottomAnchor, constant: 30),
+            tableView.leadingAnchor.constraint(equalTo: playerView.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: playerView.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        setupVideoPlayer()
+        
+        requestSend()
+        
+    }
+    
+    private func requestSend() {
         let client = APIClient.shared
         do {
             try client.getMatchVideo(matchId: self.matchId).subscribe(
@@ -53,10 +101,9 @@ class ViewController: UIViewController {
                     self.firstHalfVideoUrl = self.firstHalfVideo!.servingURL
                     self.secondHalfVideoUrl = self.secondHalfVideo!.servingURL
                     
-                    print(self.firstHalfVideoUrl)
-                    print(self.secondHalfVideoUrl)
-                    
                     self.videoURL = URL(string: "\(self.firstHalfVideoUrl)")
+                    
+//                    self.setupVideoPlayer()
                     
                 }, onError: {
                     error in
@@ -67,13 +114,75 @@ class ViewController: UIViewController {
         } catch {
             print(error)
         }
-        
-        setupVideoPlayer()
-        
     }
     
     private func setupVideoPlayer() {
-        videoPlayer.add(to: self.view)
+        videoPlayer.add(to: self.playerView)
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        // return NO to disallow editing.
+        print("TextField should begin editing method called")
+        return true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // became first responder
+        print("TextField did begin editing method called")
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+        print("TextField should snd editing method called")
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+        print("TextField did end editing method called")
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        // if implemented, called in place of textFieldDidEndEditing:
+        print("TextField did end editing with reason method called")
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // return NO to not change text
+        print("While entering the characters this method gets called")
+        return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        // called when clear button pressed. return NO to ignore (no notifications)
+        print("TextField should clear method called")
+        return true
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // called when 'return' key pressed. return NO to ignore.
+        print("TextField should return method called")
+        // may be useful: textField.resignFirstResponder()
+        return true
     }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myTableViewCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
+    
+}
