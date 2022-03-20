b9f77335b9aac63d73d040c737a8dc57ed87eb50
efemaz <efe.mazlumoglu@gmail.com>
2022-03-20 14:15:41 +0300

    app delegate root view controller comment

--- bepro-ios-task/bepro-ios-task/AppDelegate.swift
+++ bepro-ios-task/bepro-ios-task/AppDelegate.swift
@@ -11,12 +11,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
+    // i use window since we are not usign storyboards however the launchscreen storyboard is still appearing i did not remove that.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .orange
-        window?.rootViewController = ViewController()
+        window?.rootViewController = ViewController() // add viewcontroller as root view controller since we do not have another view controller
         return true
     }
     
