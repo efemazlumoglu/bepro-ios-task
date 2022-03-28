89c8368b06e35ff7c27a3b179ab21a17dfc36766
efemaz <efe.mazlumoglu@gmail.com>
2022-03-28 14:07:05 +0300

    tableview move to another class

---
+++ bepro-ios-task/bepro-ios-task/ViewController+TableView.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController+TableView.swift
+//  bepro-ios-task
+//
+//  Created by Efe Mazlumoğlu on 28.03.2022.
+//
+
+import Foundation
+import UIKit
+
+//MARK: ViewController extension for TableViewDelegate and Datasource
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.listOfOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = self.listOfOptions[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.white
+        cell.selectionStyle = .none
+        var config = cell.defaultContentConfiguration() // when you are using default table view cell you have to use config cause textLabel etc is deprecated.
+        if (data == "First Half") {
+            config.text = self.firstHalfVideoTitle
+        } else {
+            config.text = self.secondHalfVideoTitle
+        }
+        if data == self.videoURL { // double check for you cannot click on the first half video title if you are already in the first half
+            cell.isUserInteractionEnabled = false
+        } else {
+            cell.isUserInteractionEnabled = true
+        }
+        cell.contentConfiguration = config
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let data = self.listOfOptions[indexPath.row]
+        if self.videoURL != data { //you cannot click on the first half video title if you are already in the first half
+            self.callHalfs(halfOption: data)
+        }
+        self.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+}
--- bepro-ios-task/bepro-ios-task/ViewController.swift
+++ bepro-ios-task/bepro-ios-task/ViewController.swift
@@ -405,6 +405,9 @@ class ViewController: UIViewController {
     
     //MARK: Next Tapped Function
     @objc func nextTapped() {
+        
+        
+        
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) { // animation for next button changes
             self.nextButton.setTitle(">>>", for: .normal)
             self.nextButton.backgroundColor = .systemGray2.withAlphaComponent(0.5)
@@ -570,45 +573,4 @@ extension ViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
-}
-
-//MARK: ViewController extension for TableViewDelegate and Datasource
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listOfOptions.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let data = self.listOfOptions[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = UIColor.white
-        cell.selectionStyle = .none
-        var config = cell.defaultContentConfiguration() // when you are using default table view cell you have to use config cause textLabel etc is deprecated.
-        if (data == "First Half") {
-            config.text = self.firstHalfVideoTitle
-        } else {
-            config.text = self.secondHalfVideoTitle
-        }
-        if data == videoURL { // double check for you cannot click on the first half video title if you are already in the first half
-            cell.isUserInteractionEnabled = false
-        } else {
-            cell.isUserInteractionEnabled = true
-        }
-        cell.contentConfiguration = config
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let data = self.listOfOptions[indexPath.row]
-        if videoURL != data { //you cannot click on the first half video title if you are already in the first half
-            self.callHalfs(halfOption: data)
-        }
-        self.tableView.reloadData()
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
-    }
-    
 }
